6ff11f6ae19d33d5cceb967ee40de987bb626e33
GARCIA ALVAREZ SILVIA ANAHI <I23050337@monclova.tecnm.mx>
2024-04-30 21:27:34 -0600

    Agregar archivos de proyecto.

---
+++ appFileControl/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace appFileControl
+{
+  partial class Form1
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			btnSelectFolder = new Button();
+			txtFolder = new TextBox();
+			btnProcessFolder = new Button();
+			txtProcessFileNumber = new TextBox();
+			btnOpenTextFile = new Button();
+			txtContent = new TextBox();
+			btnSaveTextFile = new Button();
+			btnSave_document = new Button();
+			SuspendLayout();
+			// 
+			// btnSelectFolder
+			// 
+			btnSelectFolder.Font = new Font("Segoe UI", 20F);
+			btnSelectFolder.Location = new Point(12, 25);
+			btnSelectFolder.Name = "btnSelectFolder";
+			btnSelectFolder.Size = new Size(253, 71);
+			btnSelectFolder.TabIndex = 0;
+			btnSelectFolder.Text = "Select Folder";
+			btnSelectFolder.UseVisualStyleBackColor = true;
+			btnSelectFolder.Click += btnSelectFolder_Click;
+			// 
+			// txtFolder
+			// 
+			txtFolder.Font = new Font("Segoe UI", 20F);
+			txtFolder.Location = new Point(291, 39);
+			txtFolder.Name = "txtFolder";
+			txtFolder.ReadOnly = true;
+			txtFolder.Size = new Size(428, 43);
+			txtFolder.TabIndex = 1;
+			// 
+			// btnProcessFolder
+			// 
+			btnProcessFolder.Font = new Font("Segoe UI", 20F);
+			btnProcessFolder.Location = new Point(746, 25);
+			btnProcessFolder.Name = "btnProcessFolder";
+			btnProcessFolder.Size = new Size(253, 71);
+			btnProcessFolder.TabIndex = 2;
+			btnProcessFolder.Text = "Process Folder";
+			btnProcessFolder.UseVisualStyleBackColor = true;
+			btnProcessFolder.Click += btnProcessFolder_Click;
+			// 
+			// txtProcessFileNumber
+			// 
+			txtProcessFileNumber.Font = new Font("Segoe UI", 20F);
+			txtProcessFileNumber.Location = new Point(1025, 39);
+			txtProcessFileNumber.Name = "txtProcessFileNumber";
+			txtProcessFileNumber.ReadOnly = true;
+			txtProcessFileNumber.Size = new Size(428, 43);
+			txtProcessFileNumber.TabIndex = 3;
+			// 
+			// btnOpenTextFile
+			// 
+			btnOpenTextFile.Font = new Font("Segoe UI", 20F);
+			btnOpenTextFile.Location = new Point(12, 136);
+			btnOpenTextFile.Name = "btnOpenTextFile";
+			btnOpenTextFile.Size = new Size(253, 71);
+			btnOpenTextFile.TabIndex = 4;
+			btnOpenTextFile.Text = "Open Text File";
+			btnOpenTextFile.UseVisualStyleBackColor = true;
+			btnOpenTextFile.Click += btnOpenTextFile_Click;
+			// 
+			// txtContent
+			// 
+			txtContent.Font = new Font("Segoe UI", 20F);
+			txtContent.Location = new Point(12, 231);
+			txtContent.Multiline = true;
+			txtContent.Name = "txtContent";
+			txtContent.ScrollBars = ScrollBars.Both;
+			txtContent.Size = new Size(1441, 506);
+			txtContent.TabIndex = 5;
+			// 
+			// btnSaveTextFile
+			// 
+			btnSaveTextFile.Font = new Font("Segoe UI", 20F);
+			btnSaveTextFile.Location = new Point(1050, 136);
+			btnSaveTextFile.Name = "btnSaveTextFile";
+			btnSaveTextFile.Size = new Size(253, 71);
+			btnSaveTextFile.TabIndex = 6;
+			btnSaveTextFile.Text = "Save Text File";
+			btnSaveTextFile.UseVisualStyleBackColor = true;
+			btnSaveTextFile.Click += btnSaveTextFile_Click;
+			// 
+			// btnSave_document
+			// 
+			btnSave_document.Font = new Font("Segoe UI", 20F);
+			btnSave_document.Location = new Point(515, 136);
+			btnSave_document.Name = "btnSave_document";
+			btnSave_document.Size = new Size(253, 71);
+			btnSave_document.TabIndex = 7;
+			btnSave_document.Text = "Create and Save";
+			btnSave_document.UseVisualStyleBackColor = true;
+			btnSave_document.Click += btnSave_document_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1370, 749);
+			Controls.Add(btnSave_document);
+			Controls.Add(btnSaveTextFile);
+			Controls.Add(txtContent);
+			Controls.Add(btnOpenTextFile);
+			Controls.Add(txtProcessFileNumber);
+			Controls.Add(btnProcessFolder);
+			Controls.Add(txtFolder);
+			Controls.Add(btnSelectFolder);
+			Name = "Form1";
+			Text = "Form1";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button btnSelectFolder;
+    private TextBox txtFolder;
+    private Button btnProcessFolder;
+    private TextBox txtProcessFileNumber;
+    private Button btnOpenTextFile;
+    private TextBox txtContent;
+    private Button btnSaveTextFile;
+		private Button btnSave_document;
+	}
+}
---
+++ appFileControl/Form1.cs
@@ -0,0 +1,142 @@
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.IO;
+
+namespace appFileControl
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnSelectFolder_Click(object sender, EventArgs e)
+		{
+			//Open a Foler browser dialog to select the folder
+			FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+			folderBrowserDialog.Description = "Select the folder";
+			folderBrowserDialog.ShowNewFolderButton = false;
+			folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+			//if the user select a folder, set the path to the textbox
+			if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+			txtFolder.Text = folderBrowserDialog.SelectedPath;
+
+		}
+
+		private void btnProcessFolder_Click(object sender, EventArgs e)
+		{
+			string[] filesInFolder = Directory.GetFiles(txtFolder.Text);
+			//show in txtProcessFileNumber the number of items in the filesInFolder Array
+			txtProcessFileNumber.Text = filesInFolder.Length + " file(s) Processed";
+			foreach (string file in filesInFolder)
+			{
+				//Get the file name
+				string fileName = Path.GetFileName(file);
+				//Get the file extension
+				string fileExtension = Path.GetExtension(file);
+				//sort the file by extension using a switch
+				switch (fileExtension)
+				{
+					case ".txt":
+					case ".xlsx":
+					case ".docx":
+					case ".pptx":
+						//Move the file to the folder to a docs
+						if (!Directory.Exists(txtFolder.Text + "\\Docs"))
+						{
+							Directory.CreateDirectory(txtFolder.Text + "\\Docs");
+						}
+						File.Move(file, txtFolder.Text + "\\Docs\\" + fileName);
+						break;
+					case ".jpg":
+					case ".jpeg":
+					case ".png":
+						//Move the file to the  folder if the folder exist else create the folder
+						if (!Directory.Exists(txtFolder.Text + "\\Pics"))
+						{
+							Directory.CreateDirectory(txtFolder.Text + "\\Pics");
+						}
+						File.Move(file, txtFolder.Text + "\\Pics\\" + fileName);
+						break;
+
+					default:
+						//Move the file to the other folder
+						if (!Directory.Exists(txtFolder.Text + "\\other"))
+						{
+							Directory.CreateDirectory(txtFolder.Text + "\\other");
+						}
+						File.Move(file, txtFolder.Text + "\\other\\" + fileName);
+						break;
+				}
+			}
+		}
+
+		private void btnOpenTextFile_Click(object sender, EventArgs e)
+		{
+			//Open a file dialog to select the file
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Filter = "Text Files|*.txt";
+			dialog.Title = "Select a Text File";
+			//if the user select a file, set the path to the textbox
+			if (dialog.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+			//Read the file and show the content in the txtContent textbox
+			txtContent.Text = File.ReadAllText(dialog.FileName);
+
+		}
+
+		private void btnSaveTextFile_Click(object sender, EventArgs e)
+		{
+			//save the content of the txtContent textbox to a new file
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.Filter = "Text Files|*.txt";
+			dialog.Title = "Save the Text File";
+			//if the user select a file, set the path to the textbox
+			if (dialog.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+			//Write the content of the txtContent textbox to the file
+			File.WriteAllText(dialog.FileName, txtContent.Text);
+
+		}
+
+		private void btnSave_document_Click(object sender, EventArgs e)
+		{
+
+			string text = txtContent.Text;
+			SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+			saveFileDialog1.Filter = "Word Document (*.docx)|*.docx";
+			saveFileDialog1.ShowDialog();
+
+
+			if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+			
+			using ( WordprocessingDocument wordDocument =
+					WordprocessingDocument.Create(saveFileDialog1.FileName, DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
+				{
+					
+					MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+					mainPart.Document = new Document();
+					Body body = new Body();
+					mainPart.Document.Append(body);
+					Paragraph paragraph = body.AppendChild(new Paragraph());
+					Run run = paragraph.AppendChild(new Run());
+					run.AppendChild(new Text(text));
+				}
+
+				MessageBox.Show("Document Saved succesfully");
+			
+
+		}
+	}
+}
